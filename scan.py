43fb57765e0f4b25f99ceccbd9320e7c291fd4d3
danrivero <DanielRivero2026@u.northwestern.edu>
2025-03-09 13:34:57 -0400

    redirect code
    still need to figure out insecure_http

--- scan.py
+++ scan.py
@@ -9,7 +9,6 @@ print(input)
 print(output)
 
 def get_ip_addresses(site, resolvers, record_type):
-
     ip_addresses = set()
 
     for resolver in resolvers:
@@ -50,6 +49,43 @@ def get_http_server(site):
     except Exception as e:
         sys.stderr.write(f"Error getting http server for {site}: {e}\n")
 
+def get_http_redirects(site):
+    try:
+        insecure_http = True
+        redirect_to_https = False
+
+        if insecure_http:
+            result = subprocess.check_output(["curl", "-I", site], timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
+            redirects = 0
+
+            while "HTTP" in result and redirects < 10:
+                index = result.find("HTTP")
+                start_index = result.find(" ", index) + 1
+                end_index = result.find(" ", start_index)
+                response_code = result[start_index:end_index]
+                print(response_code)
+                if not response_code.startswith("3"):
+                    break
+
+                redirect_index = result.lower().find("location:") + 10
+                end_redirect_index = result.find("\n", redirect_index)
+                redirect = result[redirect_index:end_redirect_index]
+                print(redirect)
+
+                if redirect.startswith("https"):
+                    redirect_to_https = True
+                    break
+                result = subprocess.check_output(["curl", "-I", redirect], timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
+                redirects += 1
+
+    except subprocess.CalledProcessError:
+        sys.stderr.write(f"Error getting redirect for {site}\n")
+
+    except Exception as e:
+        sys.stderr.write(f"Error getting redirect for {site}: {e}\n")
+
+    return insecure_http, redirect_to_https
+
 resolvers_file = "public_dns_resolvers.txt"
 with open(resolvers_file, "r") as f:
     resolvers = [line.strip() for line in f if line.strip()]
@@ -65,7 +101,9 @@ with open(input, "r") as f:
             #site_dictionary["ipv4_addresses"] = get_ip_addresses(site, resolvers, "A")
             #site_dictionary["ipv6_addresses"] = get_ip_addresses(site, resolvers, "AAAA")
             site_dictionary["http_server"] = get_http_server(site)
-
+            insecure_http, redirect_to_https = get_http_redirects(site)
+            site_dictionary["insecure_http"] = insecure_http
+            site_dictionary["redirect_to_https"] = redirect_to_https
             json_dictionary[site] = site_dictionary
 
 with open(output, "w") as g:
